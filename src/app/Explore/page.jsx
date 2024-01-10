bce201bc430cf43c51944dfa7a9af443a32f71d9
Hemant2335 <knrt73373@gmail.com>
2024-01-10 21:23:31 +0530

    Explore_page_added

---
+++ src/app/Explore/page.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import sticker from "../../Assets/sticker.png";
+import poster from "../../Assets/poster.png";
+import Image from "next/image";
+import StateContext from "@/context/Context";
+import { useContext } from "react";
+import { ItemCard } from "../components";
+
+const Explore = () => {
+  const { Sticker, Poster } = useContext(StateContext);
+  const [data, setdata] = useState(null);
+
+  useEffect(() => {
+    setdata(Sticker);
+  }, []);
+
+  const handlebuttonclick = (item) => {
+    setdata(item);
+  };
+
+  return (
+    <div className="">
+      {/* headers */}
+      <div className="w-full flex flex-col items-center md:flex-row mt-[5vh]  justify-center gap-[5vw]">
+        <div
+          className="bg-[#222222] gap-[2vh] flex items-center justify-center rounded-lg hover:scale-105 transition-transform cursor-pointer w-fit h-fit p-[2vh] md:p-[3vh]"
+          onClick={() => {
+            handlebuttonclick(Sticker);
+          }}
+        >
+          <div className=" w-[5vh] overflow-hidden md:flex items-center justify-center  md:h-[5vh] rounded-lg ">
+            <Image
+              layout="responsive"
+              src={sticker}
+              width={100}
+              height={100}
+              className="rounded-xl"
+            />
+          </div>
+          <h1 className="md:text-xl font-bold text-center">Stickers</h1>
+        </div>
+        <div
+          className="bg-[#222222] gap-[2vh] flex items-center justify-center rounded-lg hover:scale-105 transition-transform cursor-pointer w-fit h-fit p-[2vh] md:p-[3vh]"
+          onClick={() => {
+            handlebuttonclick(Poster);
+          }}
+        >
+          <div className=" w-[5vh] overflow-hidden md:flex items-center justify-center  md:h-[5vh] rounded-lg ">
+            <Image
+              layout="responsive"
+              src={poster}
+              width={100}
+              height={100}
+              className="rounded-xl"
+            />
+          </div>
+          <h1 className="md:text-xl font-bold text-center">Posters</h1>
+        </div>
+      </div>
+
+      {/* Shopping Items */}
+      <div className="w-full h-fit">
+        <div className="grid grid-cols-1 md:grid-cols-5">
+          {data?.map((item) => {
+            return <ItemCard data={item} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
--- src/app/components/MoreCard.jsx
+++ src/app/components/MoreCard.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const MoreCard = () => {
+
+  const router = useRouter();
+
   return (
-    <div className="mt-5 mb-10 flex items-center justify-center rounded-lg  w-full md:w-fit bg-[#080806] p-4 md:min-w-[10vw]" > 
+    <div className="mt-5 mb-10 flex items-center justify-center rounded-lg  w-full md:w-fit bg-[#080806] p-4 md:min-w-[10vw]" onClick={()=>{router.push("/Explore")}}> 
         <h1 className='hover:underline cursor-pointer font-bold'>See More</h1>
     </div>
   )
