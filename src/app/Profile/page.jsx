9a1d23971de2598f212ee847bad87e8bb43a5719
Hemant2335 <knrt73373@gmail.com>
2024-01-16 22:11:45 +0530

    Profile_page_added

--- src/app/Profile/page.jsx
+++ src/app/Profile/page.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+"use client"
+
+import React from "react";
+import profile from "../../Assets/man-avatar.png"
+import { useState, useEffect } from "react";
+import Image from "next/image";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const [data, setdata] = useState("");
+  const show = async () => {
+    const response = await fetch(
+      `https://theprintbackend.vercel.app/users/getuser`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          auth: localStorage.getItem("token"),
+        },
+      }
+    );
 
-export default Profile
+    const json = await response.json();
+    setdata(json?.User);
+    console.log(data);
+  };
+  useEffect(() => {
+    show();
+  }, []);
+
+  return (
+    <>
+      <div className="flex justify-center my-[10vh]">
+        <div className="h-fit w-fit shadow-3xl flex flex-col md:flex-row gap-[10vh] justify-center items-center px-10 py-10">
+          <div className="shadow-3xl w-fit p-4 rounded-lg">
+            {/* {data?.isAdmin ? () : (<img src={profile} alt="" className='h-[30vh]' />)} */}
+            <Image
+              src={profile}
+              width={200}
+              height={200}
+              className="rounded-xl md:max-h-[28vh] responsive"
+            />
+          </div>
+          <div className="flex flex-col gap-5 mb-10">
+            <div className="flex gap-5">
+              <h2 className="text-xl font-poppins font-semibold">Profile </h2>
+              {data?.isAdmin ? (
+                <h2 className="text-xl font-poppins font-medium">Admin</h2>
+              ) : (
+                <h2 className="text-xl font-poppins font-medium">User</h2>
+              )}
+            </div>
+            <div className="flex gap-5">
+              <h2 className="text-xl font-poppins font-semibold">Name </h2>
+              <h2 className="text-xl font-poppins font-medium">{data?.Name}</h2>
+            </div>
+            <div className="flex gap-5">
+              <h2 className="text-xl font-poppins font-semibold">Email</h2>
+              <h2 className="text-xl font-poppins font-medium">
+                {data?.Email}
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
