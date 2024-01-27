a28f5c8d64409e0ddb86bcbc95ddf9746af2c2fd
Hemant2335 <knrt73373@gmail.com>
2024-01-27 18:46:08 +0530

    Added_filter_Dashboard

--- src/app/Explore/page.jsx
+++ src/app/Explore/page.jsx
@@ -19,28 +19,31 @@ const Explore = () => {
   const [data, setdata] = useState(null);
   const [isLoading, setisLoading] = useRecoilState(loadingstatus);
   const [Categorydata, setCategorydata] = useState(null);
-  
-  const [Category, setCategory] = useState(null);
-  
 
-  const handleAddCategoryfilter = async () => {
-    if (!Category) {
-      // setdata(Poster);
-      return;
-    }
+  const [Category, setCategory] = useState("");
+  const [Subcategory, setSubcategory] = useState("");
+  const [Type, setType] = useState("Sticker");
+
+  const handleUrl = async () => {
     try {
-      let newfile = data.filter((item) => {
-        return item.Category === Category;
-      });
-      setdata(newfile);
+      console.log(Category, Subcategory, Type);
+      const res = await fetch(
+        `https://theprintbackend.vercel.app/products/all/filter?type=${Type}&&category=${Category}&&subcategory=${Subcategory}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data = await res.json();
+      console.log("I am filter", data);
+      setdata(data?.data);
     } catch (error) {
       toast.error("Something went wrong");
     }
   };
-
-  useEffect(() => {
-    handleAddCategoryfilter();
-  }, [Category]);
 
   const handleCategory = async () => {
     try {
@@ -62,70 +65,37 @@ const Explore = () => {
   };
 
   useEffect(() => {
-    setdata(Sticker);
     handleCategory();
-  }, [Sticker]);
-
-  const handlebuttonclick = (item) => {
-    setdata(item);
-    setCategory(null);
-  };
+    handleUrl();
+  }, [Category, Subcategory, Type]);
 
   return (
     <>
       {isLoading && <Loading />}
-      <div className="md:flex gap-[2vw]">
+      <div className="md:flex gap-[2vw] ">
         {/* headers */}
-        <div className="shadow-xl rounded-lg">
-          <div className="w-full text-gray-800 flex flex-col items-center  mt-[5vh] min-w-[20vw]  justify-center gap-2">
-            <div
-              className=" shadow-3xl gap-[2vh] flex items-center justify-center rounded-lg hover:scale-105 transition-transform cursor-pointer w-fit h-fit p-[2vh] md:p-[3vh]"
-              onClick={() => {
-                handlebuttonclick(Sticker);
-              }}
-            >
-              <div className=" w-[5vh] overflow-hidden md:flex items-center justify-center  md:h-[5vh] rounded-lg ">
-                <Image
-                  layout="responsive"
-                  src={sticker}
-                  width={100}
-                  height={100}
-                  className="rounded-xl"
-                />
-              </div>
-              <h1 className="md:text-xl font-bold text-center">Stickers</h1>
-            </div>
-            <div
-              className=" gap-[2vh] shadow-3xl flex items-center justify-center rounded-lg hover:scale-105 transition-transform cursor-pointer w-fit h-fit p-[2vh] md:p-[3vh]"
-              onClick={() => {
-                handlebuttonclick(Poster);
-              }}
-            >
-              <div className=" w-[5vh]  overflow-hidden md:flex items-center justify-center  md:h-[5vh] rounded-lg ">
-                <Image
-                  layout="responsive"
-                  src={poster}
-                  width={100}
-                  height={100}
-                  className="rounded-xl"
-                />
-              </div>
-              <h1 className="md:text-xl font-bold text-center">Posters</h1>
-            </div>
-            <Filter Category={Category} setCategory={setCategory} Categorydata={Categorydata}/>
-            
-          </div>
+        <div className="shadow-xl rounded-lg h-fit p-5 min-w-[15vw]">
+          <Filter
+            Subcategory={Subcategory}
+            Type={Type}
+            setSubcategory={setSubcategory}
+            Category={Category}
+            setCategory={setCategory}
+            setdata={setdata}
+            Categorydata={Categorydata}
+            setType={setType}
+          />
         </div>
-        <hr />
-        {/* Shopping Items */}
         <div className="w-full h-fit mt-[5vh]">
-          <div className="grid grid-cols-2 md:grid-cols-5 gap-2">
-            {data?.map((item) => {
-              return <ItemCard data={item} />;
-            })}
-          </div>
+        <div className="grid grid-cols-2 md:grid-cols-5 gap-2">
+          {data?.map((item) => {
+            return <ItemCard data={item} />;
+          })}
         </div>
       </div>
+      </div>
+      {/* Shopping Items */}
+      
     </>
   );
 };
--- src/app/components/Filter.jsx
+++ src/app/components/Filter.jsx
@@ -1,12 +1,22 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { FiChevronDown } from "react-icons/fi";
 
-const Filter = ({Category ,setCategory , Categorydata}) => {
-
-  const [Subcategory, setSubcategory] = useState(null);
+const Filter = ({
+  Category,
+  Type,
+  setCategory,
+  setType,
+  Categorydata,
+  Subcategory,
+  setSubcategory,
+}) => {
   const [isSubcatdropdown, setisSubcatdropdown] = useState(false);
   const [isCatdropdown, setisCatdropdown] = useState(false);
+  const [isTypedropdown, setisTypedropdown] = useState(false);
   const [SubCategorydata, setSubCategorydata] = useState(null);
+  const Typedata = ["Poster", "Sticker"];
+
+  // Function to handle the Type filter
 
   return (
     <div>
@@ -14,6 +24,46 @@ const Filter = ({Category ,setCategory , Categorydata}) => {
       <div className="mt-[2vh]">
         <h1 className="text-gray-800 font-bold text-xl">Filters</h1>
         <div className=" gap-[2vw]">
+          <div className="mt-[2vh]">
+            <h1
+              className="text-gray-800 font-bold shadow-3xl w-fit p-2 cursor-pointer rounded-lg flex items-center"
+              onClick={() => {
+                setisTypedropdown(!isTypedropdown);
+                setisSubcatdropdown(false);
+                setisCatdropdown(false);
+              }}
+            >
+              {Type ? `${Type}` : "Select Type"} <FiChevronDown />
+            </h1>
+            {isTypedropdown && (
+              <div className="mt-[2vh] z-50 shadow-3xl max-h-[20vh] overflow-y-auto absolute bg-white rounded-lg">
+                <div
+                  className=" p-2 cursor-pointer hover:bg-red-400 rounded-md flex items-center"
+                  onClick={() => {
+                    setType("");
+                    setisTypedropdown(false);
+                  }}
+                >
+                  <h1 className="text-gray-800 font-bold">Select Type</h1>
+                </div>
+                {Typedata?.map((item) => {
+                  return (
+                    <div
+                      className=" p-2 cursor-pointer hover:bg-red-400 rounded-md flex items-center"
+                      onClick={() => {
+                        setType(item);
+                        setisCatdropdown(false);
+                        setisSubcatdropdown(false);
+                        setisTypedropdown(false);
+                      }}
+                    >
+                      <h1 className="text-gray-800 font-bold">{item}</h1>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
           <div className="mt-[2vh]">
             <h1
               className="text-gray-800 font-bold shadow-3xl w-fit p-2 cursor-pointer rounded-lg flex items-center"
@@ -29,9 +79,10 @@ const Filter = ({Category ,setCategory , Categorydata}) => {
                 <div
                   className=" p-2 cursor-pointer hover:bg-red-400 rounded-md flex items-center"
                   onClick={() => {
-                    setCategory(null);
+                    setCategory("");
                     setisCatdropdown(false);
-                    setSubcategory(null);
+                    setSubcategory("");
+                    setSubCategorydata(null);
                   }}
                 >
                   <h1 className="text-gray-800 font-bold">Select Category</h1>
@@ -43,8 +94,8 @@ const Filter = ({Category ,setCategory , Categorydata}) => {
                       onClick={() => {
                         setCategory(item?.Name);
                         setisCatdropdown(false);
-                        setSubcategory(null);
-
+                        setSubcategory("");
+                        
                         setSubCategorydata(item?.subcategory);
                       }}
                     >
@@ -69,6 +120,15 @@ const Filter = ({Category ,setCategory , Categorydata}) => {
               </h1>
               {isSubcatdropdown && (
                 <div className="mt-[2vh] max-h-[20vh] z-50 overflow-y-auto shadow-3xl absolute bg-white rounded-lg">
+                   <div
+                  className=" p-2 cursor-pointer hover:bg-red-400 rounded-md flex items-center"
+                  onClick={() => {
+                    setSubcategory("");
+                    setisSubcatdropdown(false);
+                  }}
+                >
+                  <h1 className="text-gray-800 font-bold">Select Subcategory</h1>
+                </div>
                   {SubCategorydata?.map((item) => {
                     return (
                       <div
